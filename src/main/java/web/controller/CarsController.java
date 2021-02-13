6f81a437abf420834ba304448db42117314c565f
Irina Park <park_irina@naver.com>
2021-02-14 02:50:33 +0900

    new commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class CarsController {
 
     @GetMapping(value = "/cars")
-    public String carsTable (@RequestParam(value = "count", defaultValue = "0") int count, Model model) {
+    public String carsTable (@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
 
         Car car1 = new Car ("Bentley", "England", 2010);
         Car car2 = new Car ("Mercedes", "Germany", 2015);
@@ -22,18 +22,7 @@ public class CarsController {
         Car car4 = new Car ("Hyundai", "Korea", 2007);
         Car car5 = new Car ("Chevrolet", "United States", 2013);
         Car[] cars = {car1, car2, car3, car4, car5};
-
-        List<Car> carsList = new ArrayList<>();
-        if (count > 0 && count < 5) {
-            for (int i = 0; i < count; i++) {
-                carsList.add(cars[i]);
-            }
-        }
-        else {
-            for (int i = 0; i < 5; i++) {
-                carsList.add(cars[i]);
-            }
-        }
+        List<Car> carsList = Arrays.stream(cars).limit(count).collect(Collectors.toList());
 
         model.addAttribute("carsList", carsList);
         return "cars";
