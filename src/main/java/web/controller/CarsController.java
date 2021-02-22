9c3fc6e8e9d0e256e3c2d889b2b9701f03da2fe5
Irina Park <park_irina@naver.com>
2021-02-23 02:55:42 +0900

    new commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Controller
 public class CarsController {
@@ -27,11 +24,3 @@ public class CarsController {
         return "cars";
     }
 }
-
-//        Car car1 = new Car ("Bentley", "England", 2010);
-//        Car car2 = new Car ("Mercedes", "Germany", 2015);
-//        Car car3 = new Car ("Toyota", "Japan", 2020);
-//        Car car4 = new Car ("Hyundai", "Korea", 2007);
-//        Car car5 = new Car ("Chevrolet", "United States", 2013);
-//        Car[] cars = {car1, car2, car3, car4, car5};
-//        List<Car> carsList = Arrays.stream(cars).limit(count).collect(Collectors.toList());
